93e4d031ccc4479f60000081f8c8d0b24b06aee5
Fernando Brasil <fernando@devbrasil.com.br>
2019-07-19 09:04:04 -0300

    Finalizado a versao final

--- func_printar_na_notacao.c
+++ func_printar_na_notacao.c
@@ -1,11 +1,13 @@
 void imprimir_arvore_binaria_na_notacao(Tree *t)
 {
-    printf(",[");
+    printf("[");
     if (!treeIsEmpty(t))
     {
 
         printf("%d", t->num);
+        printf(",");
         imprimir_arvore_binaria_na_notacao(t->sae);
+        printf(",");
         imprimir_arvore_binaria_na_notacao(t->sad);
         printf("]");
     }
